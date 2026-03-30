57ba14de590ece2cccb363a3a3fd352d42e0ee81
Lachu Leo <lakshmanan.unq@gmail.com>
2026-03-30 23:57:39 +0530

    fix base url

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 
-const BASE_URL = import.meta.env.VITE_API_URL
+const BASE_URL = 'https://task-backend-jc5d.onrender.com'
 
 function App() {
   const [email, setEmail] = useState("")
