477a23b0dce364d9182a76374ab4452f1402fa16
Lachu Leo <lakshmanan.unq@gmail.com>
2026-03-30 23:24:42 +0530

    frontend ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 
-const BASE_URL = "http://localhost:3001"
+const BASE_URL = import.meta.env.VITE_API_URL
 
 function App() {
   const [email, setEmail] = useState("")
